c02b8228339d6eebdc82ea87186fbdea8533e5d3
maleavle <maleavle@gmail.com>
2015-03-18 22:19:02 -0500

    ola k ase
    jojsa

--- menu.php
+++ menu.php
@@ -8,28 +8,7 @@ $numClientes = count($cliente);
 $redes = Redes::listado();
 $numRedes = count($redes);
 ?>
-<div id="navTop">
-    <ul class="registro">
-        <?php
-        if( !isset($_SESSION["usuarioTempo"]) ){?>
- 			<li><a href="index.php?pag=registro">Registrarse</a></li>
-        	<li><a id="inline" href="#data" class="cuenta">Mi Cuenta</a></li>
-		<?php }
-		else {
-			echo('
-			<li style="padding-right:50px;"><a href="index.php?pag=perfil">Mi perfil</a></li>
-			<li><a href="index.php?pag=logout" class="cuenta">Logout</a></li>');	
-		}
-		?>
-        </li>
-    </ul>
-	<?php if($numRedes != 0 ){
-		for($i = 0; $i < $numRedes ; $i++ ){?>
-			<a class="redes" href="<?php echo($redes[$i]->getLink()); ?>" target="_blank">
-				<img src="images/redes/<?php echo($redes[$i]->getThumb()); ?>" title="<?php echo($redes[$i]->getNombre()); ?>" alt="<?php echo($redes[$i]->getNombre()); ?>" />	
-			</a>
-    <?php }}?>
-</div>
+
 <div class="menu">
 	<a href="index.php"><img class="logo" src="images/logo-footer.png" alt=""/></a>
 	
@@ -41,6 +20,5 @@ $numRedes = count($redes);
     <a class="link" href="index.php?pag=contacto" <?php if($pagina == 'contacto'){ ?>class="select"<?php } ?>>CONTÁCTENOS</a>
 	
 
-	<!-- <a class="redes space fb" href="<?php //echo $general->fb ?>" target="_blank" ><img src="/images/basic/fb.png" alt="accesorios y acabados" /></a>
-	<a class="redes tw" href="<?php //echo $general->twitter ?>" target="_blank"><img src="/images/basic/tw.png" alt="accesorios y acabados" /></a> -->
+
 </div>
