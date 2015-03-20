245f5054ce2bf509b5497a9432fbe9f921f357a3
jhon camargo <jhon.pirry03@gmail.com>
2015-03-19 22:47:00 -0500

    img user y principal

---
+++ principal.php
@@ -0,0 +1,32 @@
+<<style>
+<!--
+.ira {
+	position: absolute;
+	right: 10px;
+	top: 5px;
+	list-style: none;
+	width: 160px;
+	height: 35px;
+}
+
+.contactenoos {
+	position: absolute;
+	right: 1340px;
+	top: 5px;
+	list-style: none;
+	width: 160px;
+	height: 35px;
+}
+-->
+</style>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+</head>
+
+<body>
+<img src="images/slide/unnamed.jpg" style="position: absolute; left:0; top: 0; width: 100%; height:100%" />
+<a class="ira" href="index.php"></div>
+<a class="contactenoos" href="index.php?pag=contacto"></div>
+</body>
+</html>
